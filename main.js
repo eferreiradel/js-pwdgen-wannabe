017ec819a255dc96b6a6ce7af9a63099a7d64767
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-04-12 21:01:21 +0200

    feat [JS]: aggiunti question prompt

---
+++ main.js
@@ -0,0 +1,8 @@
+let nome = prompt("Come ti chiami?");
+let eta = prompt("Quanti anni hai?");
+let animale = prompt("Come si chiama il tuo animale domestico?");
+
+const pwd = nome + eta + animale;
+
+alert(`ciao ${nome}`);
+alert(`La tua password sicurissima è ${pwd} tienila al sicuro ;)`);
