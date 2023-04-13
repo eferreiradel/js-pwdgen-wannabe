1cf81b4915f30a8ec5aa54c37c539ad400fb7d9d
eferreiradel <cont.limaferreira.everton@gmail.com>
2023-04-13 10:04:38 +0200

    feat {JS}: aggiunto text pwd.

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ let nome = prompt("Come ti chiami?");
 let eta = prompt("Quanti anni hai?");
 let animale = prompt("Come si chiama il tuo animale domestico?");
 
-const pwd = nome + eta + animale;
+let pwd = `${nome}-#${eta}_?${animale}@_"2023"`;
+
+document.getElementById("password").innerText = pwd;
 
 alert(`ciao ${nome}`);
 alert(`La tua password sicurissima è ${pwd} tienila al sicuro ;)`);
